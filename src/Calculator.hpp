c19055785e82a0d447eb6ce7f1846887de0898eb
CodeMouse179 <codemouse179@gmail.com>
2023-05-01 00:00:46 +0300

    commit

--- src/Calculator.hpp
+++ src/Calculator.hpp
@@ -1,10 +1,31 @@
+#define SYSTEM_STRING_CONSOLE
 #include "String.hpp"
 
 namespace CodeMouse
 {
+    enum class CalculationResult
+    {
+        Success = 0,
+        Fail = 1,
+    };
+
     class Calculator
     {
     public:
+        static bool PreProcess(const std::string& expression)
+        {
+            if (StringA::IsNullOrWhiteSpace(expression))
+            {
+                return false;
+            }
+            return true;
+        }
 
+        static std::string Process(const std::string& expression, CalculationResult& calcRet)
+        {
+            std::string result;
+            calcRet = CalculationResult::Success;
+            return result;
+        }
     };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,29 @@
 
 int main()
 {
+    while(true)
+    {
+        std::string expression = Console::ReadLine();
+        bool suc = CodeMouse::Calculator::PreProcess(expression);
+        if (!suc)
+        {
+            Console::WriteLine(TEXT("wrong"));
+            continue;
+        }
+        if (StringA::Equals(expression, "exit"))
+        {
+            break;
+        }
+        CodeMouse::CalculationResult cr;
+        std::string result = CodeMouse::Calculator::Process(expression, cr);
+        if (cr == CodeMouse::CalculationResult::Success)
+        {
+            Console::WriteLine(result);
+        }
+        else if (cr == CodeMouse::CalculationResult::Fail)
+        {
+            Console::WriteLine(result);
+        }
+    }
     return 0;
 }
